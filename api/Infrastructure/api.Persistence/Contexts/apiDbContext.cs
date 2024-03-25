2e187ffd9e38c3605a183a2e99da25f85a4a909b
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-03-26 01:24:00 +0300

    SaveChangeAsync Interceptor added

--- api/Infrastructure/api.Persistence/Contexts/apiDbContext.cs
+++ api/Infrastructure/api.Persistence/Contexts/apiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using api.Domain.Entities;
+using api.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -16,5 +17,25 @@ namespace api.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //ChangeTracker: Entity üzerinde değişikliklerin veya eklenenlerin yakalanmasını sağlar. Track edilen verileri yakalayıp elde eder.
+
+            //herhangi bir save işleminde override ile save sürecinde araya girip added veya modified durumuna göre ortak entity createddate ve updateddate değerlerine işlem
+            var datas = ChangeTracker
+                .Entries<BaseEntity>();
+
+            foreach(var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow,
+                };
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- api/Presentation/api.webapi/Controllers/ProductsController.cs
+++ api/Presentation/api.webapi/Controllers/ProductsController.cs
@@ -12,52 +12,98 @@ namespace api.webapi.Controllers
         readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
 
-        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        readonly private IOrderWriteRepository _orderWriteRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
+
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IOrderWriteRepository orderWriteRepository, ICustomerWriteRepository customerWriteRepository, IOrderReadRepository orderReadRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository = customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
         }
+
 
         [HttpGet]
-        public async Task Get()
-        {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Product 1",
-                    Stock = 100,
-                    CreatedDate= DateTime.UtcNow,
-                    Price = 1000
-                },
+      public async Task Get()
+      {
+           Order order = await _orderReadRepository.GetByIdAsync("512debe6-7f8d-4f94-8207-1b7b63252777");
+           order.Address = "Kütahya, Tavşanlı";
+           await _orderWriteRepository.SaveAsync();
+      }
 
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Product 2",
-                    Stock = 200,
-                    CreatedDate= DateTime.UtcNow,
-                    Price = 2000
-                },
 
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Product 3",
-                    Stock = 300,
-                    CreatedDate= DateTime.UtcNow,
-                    Price = 3000
-                }
-            });
-            await _productWriteRepository.SaveAsync();
-        }
 
+/*
+  [HttpGet]
+  public async Task Get()
+  {
+      var customerId = Guid.NewGuid();
+      await _customerWriteRepository.AddAsync(new() { Id = customerId, Name ="Eray"});
+      await _orderWriteRepository.AddAsync(new() { Description = "bla bla", Address = "Ankara, Çankaya", CustomerId= customerId });
+      await _orderWriteRepository.AddAsync(new() { Description = "bla bla 2", Address = "Kütahya, Merkez", CustomerId = customerId });
+      await _orderWriteRepository.SaveAsync();
+  }
+  /*
+
+  /*
+  [HttpGet]
+  public async Task Get()
+  {
+      await _productWriteRepository.AddAsync(
+          new() { Name = "C Product", Price = 1.500F, Stock = 10, CreatedDate=DateTime.UtcNow }
+          );
+      await _productWriteRepository.SaveAsync();
+  }
+  /*
+
+
+  [HttpGet]
+  public async Task Get()
+  {
+      await _productWriteRepository.AddRangeAsync(new()
+      {
+          new()
+          {
+              Id = Guid.NewGuid(),
+              Name = "Product 1",
+              Stock = 100,
+              CreatedDate= DateTime.UtcNow,
+              Price = 1000
+          },
+
+          new()
+          {
+              Id = Guid.NewGuid(),
+              Name = "Product 2",
+              Stock = 200,
+              CreatedDate= DateTime.UtcNow,
+              Price = 2000
+          },
+
+          new()
+          {
+              Id = Guid.NewGuid(),
+              Name = "Product 3",
+              Stock = 300,
+              CreatedDate= DateTime.UtcNow,
+              Price = 3000
+          }
+      });
+      await _productWriteRepository.SaveAsync();
+  }
+  */
+
+        /*
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
         {
             Product product = await _productReadRepository.GetByIdAsync(id);
             return Ok(product);
         }
+        */
     }
 }
