0f5f6ead21375d5c1c85badc6cc030413e3c29f4
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-03-26 03:03:59 +0300

    fix

--- api/Infrastructure/api.Persistence/Contexts/apiDbContext.cs
+++ api/Infrastructure/api.Persistence/Contexts/apiDbContext.cs
@@ -32,6 +32,7 @@ namespace api.Persistence.Contexts
                 {
                     EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
                     EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow,
+                    _ => DateTime.UtcNow
                 };
             }
 
