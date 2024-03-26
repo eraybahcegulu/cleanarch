ce08234df4498371a88ce6297e3f4e7572d8923c
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-03-26 14:52:02 +0300

    FluentValidate

---
+++ api/Core/api.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,33 @@
+﻿using api.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace api.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Product name required")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                    .WithMessage("Product name must between 5-50 characters");
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Product stock required")
+                .Must(s => s >= 0)
+                    .WithMessage("Stock must be 0 or greater");
+
+            RuleFor(p => p.Price)
+                 .NotEmpty()
+                 .NotNull()
+                     .WithMessage("Product price required")
+                 .Must(s => s >= 0)
+                     .WithMessage("Price must be 0 or greater");
+        }
+    }
+}
--- api/Presentation/api.webapi/Program.cs
+++ api/Presentation/api.webapi/Program.cs
@@ -1,4 +1,7 @@
+using api.Application.Validators.Products;
+using api.Infrastructure.Filters;
 using api.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +11,10 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
     policy.WithOrigins("http://localhost:3000", "https://localhost:3000").AllowAnyHeader().AllowAnyMethod()
 ));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+     .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>()) //mimari ile tek bir tanýmlama(CreateProductValidator) ile tüm validatorlar iþlenir
+     .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter= true); // default filter bastýrýlýyor custom filter tasarlamak için
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
